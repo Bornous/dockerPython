7c72a215e9491d5b2f094ad39c9f6ba180660afb
Bornous <pavelgra77@gmail.com>
2019-06-02 21:43:08 +0200

    Sql working nice

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,16 @@ RUN pip3 install --upgrade pip
 
 #Install app depencies
 RUN pip3 install Flask
+RUN sudo apt install python3-pip
+RUN sudo su
+student
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+RUN curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+RUN exit
+RUN sudo apt-get UPDATE
+RUN sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+RUN sudo apt install unixodbc-dev
+RUN pip3 install --user pyodbc
 
 #Bundle app source
 COPY simpleapp.py /src/simpleapp.py
--- simpleapp.py
+++ simpleapp.py
@@ -1,19 +1,36 @@
 from flask import Flask
 import sys
 import optparse
+import pyodbc
+from datetime import date
 import time
-
 app = Flask(__name__)
 
 start = int(round(time.time()))
 
 @app.route("/")
 def hello_world():
-    return "Hello world from Distelli & Docker!"
+    try:
+        conn = pyodbc.connect(
+            r'DRIVER={ODBC Driver 17 for SQL Server};'
+            r'SERVER=10.0.2.2,1433;'
+            r'DATABASE=dbo_testing;'
+            #r'Trusted_Connection=yes;'
+            r'UID=pythonapp;'
+            r'Pwd=dockerPython17;'
+        )
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM dbo.testing")
+        my_str =""
+        for row in cursor.fetchall():
+            my_str= my_str+"<div>"+str(row)+"</div>"
+        return "Connected correctly! At: "+str(date.today())+" "+my_str
+    except pyodbc.Error:
+        return "Connection with sql server failed!!! At: "+str(date.today())+" "
 
 @app.route("/doc_choose")
 def doc_choose():
-    return "<h1>Wybierz raport:</h1><div>Zmiana w pensum</div>"
+    return "<h1>Wybierz raport:</h1><div><a href=\"/\">Zmiana w pensum</a></div>"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python3 simpleapp.py -p ")
